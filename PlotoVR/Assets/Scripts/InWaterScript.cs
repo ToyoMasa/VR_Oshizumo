79b3cb69b6fce580be944b52921873171bf3d1ec
MYCOMPUTER\dqdab <to46ra1001ma3104@gmail.com>
2019-11-14 15:45:02 +0900

    頂いたプロトタイプ版です。

---
+++ PlotoVR/Assets/Scripts/InWaterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class InWaterScript : MonoBehaviour
+{
+    public Transform Target;
+
+    ColorGradingComponent ColorGrading;
+    PostProcessingContext Post;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ColorGrading = GetComponent<ColorGradingComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlotoVR/Assets/Scripts/SceneReset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+
+public class SceneReset : MonoBehaviour
+{
+    [SerializeField]
+    private SteamVR_Input_Sources hand_type_;
+
+    [SerializeField]
+    private SteamVR_Action_Boolean hand_action_;
+
+    int count = 0;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || hand_action_.GetState(hand_type_))
+        {
+            count++;
+        }
+        else
+        {
+            count = 0;
+        }
+
+        if (count > 60)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
