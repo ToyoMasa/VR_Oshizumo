8e93f0189ff9b2e5a6440b0236ca4043b7f54ec5
--ARIL--\有坂　祐哉 <unko@yahoo.co.jp>
2019-11-28 15:22:00 +0900

    水中表現

---
+++ PlotoVR/Assets/Scripts/InWater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityEngine.UI;
+//using UnityStandardAssets.ImageEffects;
+using UnityEditor;
+
+
+public class InWater : MonoBehaviour
+{
+
+    //　水ゲームオブジェクト
+    public Transform water;
+    //　水中表現にUIのパネルを使用する場合は設定
+    public GameObject waterPanel;
+    //　カメラが水と接触しているかどうか
+    private bool isInWater = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( transform.position.y <= water.position.y  )
+        {
+            waterPanel.SetActive(true);
+        }
+        isInWater = true;
+    }
+}
