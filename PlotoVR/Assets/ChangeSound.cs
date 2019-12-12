5fa6c7e3c1ee94ae2931fd4906a15a5a701c77ee
DESKTOP-NSGFA9R\古内　賢 <Furuuti.3223@gmail.com>
2019-12-12 15:03:05 +0900

    SoundeCommanderをプレイヤーに入れてください

---
+++ PlotoVR/Assets/ChangeSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AudioClip Birdvoice;     //環境音鳥の鳴き声
+    public AudioClip Wind;
+    AudioSource Ringer;
+    
+    void Start()
+    {
+        Ringer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+
+        if (pos.y >= 2 && !Ringer.isPlaying) {
+            Ringer.PlayOneShot(Wind);
+            Ringer.PlayOneShot(Birdvoice);
+        }
+        if (pos.y <= 2)
+        {
+            Ringer.Stop();
+        }
+
+
+
+    }
+}
---
+++ PlotoVR/Assets/UnderwaterSounde.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderwaterSounde : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip Water ;     //水中呼吸音
+    public AudioClip Wave  ;
+    public AudioClip Landing ;
+    AudioSource UnderWaterRinger;
+    AudioSource InWaterRinger;
+    void Start()
+    {
+        UnderWaterRinger = GetComponent<AudioSource>();
+        InWaterRinger = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+        if ( pos.y <= -50 && !UnderWaterRinger.isPlaying)
+        {
+            UnderWaterRinger.PlayOneShot(Water);
+            UnderWaterRinger.PlayOneShot(Wave);
+        }
+        if (pos.y <= -50 && pos.y>=-55 && !InWaterRinger.isPlaying)
+        {
+            InWaterRinger.PlayOneShot(Landing);
+        }
+        if(pos.y >= -50 && pos.y <= -60)
+        {
+            InWaterRinger.Stop();
+        }
+
+            if (pos.y >= -60)
+        {
+            UnderWaterRinger.Stop();
+        }
+    }
+}
